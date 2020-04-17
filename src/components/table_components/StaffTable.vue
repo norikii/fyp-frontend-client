4221d20b02c940d51fff5123e058b4ba1507e147
tatrasoft <n.nazarej@gmail.com>
2020-04-17 10:09:33 +0100

    mixins field removed

--- src/components/table_components/StaffTable.vue
+++ src/components/table_components/StaffTable.vue
@@ -116,9 +116,6 @@
 
     export default {
         name: "StaffTable",
-        mixins: [
-            unix_to_date_mixin,
-        ],
         components: {
             AddStaffUserForm,
         },
