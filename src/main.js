aa47ca58e3e22592fcaa11b50b3bf7e0a152df9e
tatrasoft <n.nazarej@gmail.com>
2020-04-19 08:46:21 +0100

    implements automatic login

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 
 require("./assets/scss/main.scss");
 
@@ -21,5 +22,24 @@ Vue.use(Buefy);
 new Vue({
   router,
   store,
+  created () {
+    const userString = localStorage.getItem('user');
+    // when the app is created we check if there is a user in our local storage
+    if (userString) {
+      // if there is we are restoring it in our sore
+      const userData = JSON.parse(userString);
+      this.$store.commit('SET_USER_DATA', userData)
+    }
+    axios.interceptors.response.use(
+        response => response,
+        error => {
+          if (error.response.status === 401) {
+            this.$store.dispatch('logout')
+          }
+          return Promise.reject(error);
+        }
+    )
+
+  },
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
