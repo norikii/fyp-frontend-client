b994c99ba7f4db025af84c19ad00b60eff8a9aa2
tatrasoft <n.nazarej@gmail.com>
2020-04-19 08:50:39 +0100

    the view is protected against unauthorized access

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,16 +1,40 @@
 <template>
     <div id="dashboard">
-        <side-bar-component></side-bar-component>
-        <data-table-component></data-table-component>
+
+
+
+        <template v-if="loggedIn">
+            <side-bar-component></side-bar-component>
+            <data-table-component></data-table-component>
+        </template>
+        <div v-else>
+            <h1>Unauthorized access</h1>
+        </div>
+
     </div>
 </template>
 
 <script>
     import SideBarComponent from "../components/SideBarComponent";
     import DataTableComponent from "../components/DataTableComponent";
+    import axios from 'axios'
+    import { authComputed } from "../store/helper";
+
     export default {
         name: "DashboardView",
         components: {DataTableComponent, SideBarComponent},
+        data() {
+            return {
+            }
+        },
+        created() {
+            axios.get('http://localhost:12345/auth/dashboard').then(({data}) => {
+                this.isLoading = false;
+            })
+        },
+        computed: {
+            ...authComputed
+        }
     }
 </script>
 
